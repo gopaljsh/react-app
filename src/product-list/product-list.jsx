562d1ecca9e7a4867b7744140856752c8c8422f9
Gopal Joshi <uday_joshi69@yahoo.com>
2019-11-06 14:28:55 +0530

    update

--- src/product-list/product-list.jsx
+++ src/product-list/product-list.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ProductItem from './product-item/product-item';
 
 class ProductList extends React.Component {
-    sortItems = () => {
-        let products = this.props.products.sort((a,b) => {
-            return a.productname.localeCompare(b.productname);
-        });
-    }
     render() {
         let product = this.props.products.map((product, index) => {
             return (
